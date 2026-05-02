eaa424f7fed205e641b19cdb7f98382f99669c07
Álvaro <agomezguemes@gmail.com>
2026-05-02 22:04:52 +0200

    feat(splash): module skeleton and barrel exports

---
+++ src/animations/splash/primitives/index.ts
@@ -0,0 +1,6 @@
+// src/animations/splash/primitives/index.ts
+export { default as SoftGlow } from './SoftGlow';
+export { default as GoldSphere } from './GoldSphere';
+export { default as GoldBlock } from './GoldBlock';
+export { default as IsoCube } from './IsoCube';
+export { default as KairosWordmark } from './KairosWordmark';
